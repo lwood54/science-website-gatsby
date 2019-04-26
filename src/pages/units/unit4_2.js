0a2d4ebe5960cd5d369a0d06bb75c5f1eae83841
Logan Wood <lwood3499@gmail.com>
2019-04-25 21:55:15 -0500

    unit page content addition

---
+++ src/pages/units/unit4_2.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../../components/layout";
+
+const Unit4_2 = () => {
+      return (
+            <Layout>
+                  <h1>Unit 4.2</h1>
+                  <h3>
+                        This will be replaced with a template that will have
+                        props passed to it from an object literal.
+                  </h3>
+            </Layout>
+      );
+};
+
+export default Unit4_2;
