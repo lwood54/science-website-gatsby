720ac743c699a06d5bab548cd06b35cb732f16ba
Logan Wood <lwood3499@gmail.com>
2019-04-26 23:09:59 -0500

    basic unit page structure

---
+++ src/pages/units/unit5_1.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../../components/layout";
+
+const Unit5_1 = () => {
+      return (
+            <Layout>
+                  <h1>Unit 5.1</h1>
+                  <h3>
+                        This will be replaced with a template that will have
+                        props passed to it from an object literal.
+                  </h3>
+            </Layout>
+      );
+};
+
+export default Unit5_1;
---
+++ src/pages/units/unit6_2.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../../components/layout";
+
+const Unit6_2 = () => {
+      return (
+            <Layout>
+                  <h1>Unit 6.2</h1>
+                  <h3>
+                        This will be replaced with a template that will have
+                        props passed to it from an object literal.
+                  </h3>
+            </Layout>
+      );
+};
+
+export default Unit6_2;
